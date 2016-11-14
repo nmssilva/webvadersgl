d53d752ada279689c6230a0becd6be29996d8c9f
Nuno Miguel Soares Silva <nuno.miguel.silva@ua.pt>
2016-11-14 19:42:00 +0000

    stufff

--- game/server.js
+++ game/server.js
@@ -1,6 +1,8 @@
-
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -9,3 +11,5 @@ app.get('/', function(req, res){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
